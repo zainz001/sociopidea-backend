52ac4b8787dae99bb7be68719f1e08e6fe30784b
zainz001 <uhanif30@gmail.com>
2023-09-25 05:40:15 -0700

    proposal

--- src/api/index.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-const url='http://localhost:5000/posts';
-
-export const fetchpost =()=> axios.get(url);
--- src/componenets/posts/post/post.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-const Post=()=>{
-    return(
-        <h1>post</h1>
-    )
-}
-export default Post;
-
