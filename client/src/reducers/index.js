38bb93d44b795598dff0e4b69ff4c9da755ce55e
zainz001 <uhanif30@gmail.com>
2023-11-06 23:22:58 +0500

    signin signu api and like according to user

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-
-import post from './posts'
-
-export default combineReducers({
-        post,
-})
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { getposts,createpost,deletepost,updatepost,like  } from '../controller/posts.js';
+import auth from'../middleware/auth.js';
+
 const router=express.Router();
 
 router.get('/post',getposts);
-router.post('/create',createpost);
-router.patch('/update/:id', updatepost);
-router.delete('/delete/:id',deletepost);
-router.patch('/like/:id',like)
+router.post('/create',auth,createpost);
+router.patch('/update/:id',auth, updatepost);
+router.delete('/delete/:id',auth,deletepost);
+router.patch('/like/:id',auth,like)
 export default router;
